99fa74902551e435b7a470ea47bde63ba6652f75
Seob Lee이준섭Jun <junseoblee96@gmail.com>
2021-04-27 16:27:59 +0900

    Deploying a React App with Netlify

---
+++ deploy_web.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+netlify build
+netlify deploy
--- packages/web/src/apollo.ts
+++ packages/web/src/apollo.ts
@@ -2,7 +2,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: "http://localhost:4000",
+    uri: process.env.REACT_APP_SERVER_URL,
     credentials: "include",
   }),
   cache: new InMemoryCache(),
